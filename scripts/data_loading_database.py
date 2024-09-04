7af4a74d470acc0c2a255ee2ce358d75267bd8e3
Rita Uzoka <54287482+Rita-cyber@users.noreply.github.com>
2024-09-04 20:00:25 +0100

    Update data_loading_database.py

--- scripts/data_loading_database.py
+++ scripts/data_loading_database.py
@@ -17,10 +17,10 @@ class CSVLoader:
 
         # Establish a connection to the PostgreSQL database
         self.conn = psycopg2.connect(
-            host=os.getenv(localhost),
-            dbname=os.getenv(NYCPayroll),
-            user=os.getenv(postgres),
-            password=os.getenv(Chinwe31#)
+            host=localhost,
+            dbname=NYCPayroll,
+            user=postgres,
+            password=Chinwe31#
         )
         self.cursor = self.conn.cursor()
 
